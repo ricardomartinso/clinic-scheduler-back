565681c7169cd5552873400e3729cb844aeb4d7f
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-09-28 21:22:30 -0300

    feat: added auth services to handle auth creations

---
+++ src/services/authServices.ts
@@ -0,0 +1,42 @@
+import { create, findByEmail } from "../repositories/authRepository";
+import { AuthUser } from "../types/authTypes";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { conflictError, unauthorizedError } from "../utils/errorUtils";
+
+export async function createUser(user: AuthUser) {
+  await verifyEmailCreation(user.email);
+
+  const encryptedUser = {
+    email: user.email,
+    password: bcrypt.hashSync(user.password, 10),
+  };
+
+  await create(encryptedUser);
+
+  return user;
+}
+
+export async function loginUser(user: AuthUser) {
+  const userData = await findByEmail(user.email);
+
+  if (!userData) return unauthorizedError("Invalid email or password");
+
+  verifyValidPassword(user.password, userData.password as string);
+
+  const token = jwt.sign({ userData }, process.env.SECRET_KEY as string);
+
+  return token;
+}
+
+async function verifyEmailCreation(email: string) {
+  const existEmail = await findByEmail(email);
+
+  if (existEmail) return conflictError("User already registered!");
+}
+
+function verifyValidPassword(userPassword: string, encryptedPassword: string) {
+  if (!bcrypt.compareSync(userPassword, encryptedPassword)) {
+    return unauthorizedError("Invalid email or password");
+  }
+}
