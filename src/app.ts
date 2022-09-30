50ec08c0b34fcfb3040e57bd42cc752025f83a6c
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-09-30 20:04:52 -0300

    fix: resolving error handler not imported

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
 import cors from "cors";
 import express from "express";
 import "express-async-errors";
-import dotenv from "dotenv";
+import { errorHandlerMiddleware } from "./middlewares/errorHandler";
 import router from "./routers/router";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 export const app = express();
 
 app.use(cors());
+
 app.use(express.json());
+
 app.use(router);
+
+app.use(errorHandlerMiddleware);
