7eab95c6d941b0373a7169d2daa1c8fac3a37b2f
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-10-03 12:10:43 -0300

    fix: added return to consultation creation

--- src/repositories/consultationRepository.ts
+++ src/repositories/consultationRepository.ts
@@ -2,7 +2,7 @@ import { prisma } from "../config/database";
 import { CreateConsultation } from "../types/consultationTypes";
 
 export async function create(data: CreateConsultation) {
-  await prisma.consultation.create({
+  return await prisma.consultation.create({
     data,
   });
 }
