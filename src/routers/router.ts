3bcc9063be967036a35b4cbec71e111e0e3533e3
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-10-03 12:09:18 -0300

    feat: added patient router on main route

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRouter from "./authRouter";
 import consultationRouter from "./consultationRouter";
+import patientRouter from "./patientRouter";
 
 const router = Router();
 
 router.use(authRouter);
 router.use(consultationRouter);
+router.use(patientRouter);
 
 export default router;
