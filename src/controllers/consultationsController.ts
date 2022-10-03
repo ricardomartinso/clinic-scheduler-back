04543fcf4edd8ecbc4e19d97b71dae0fa7ba08c5
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-10-03 12:10:17 -0300

    feat: added controller for consultation creation

--- src/controllers/consultationsController.ts
+++ src/controllers/consultationsController.ts
@@ -2,5 +2,14 @@ import { Request, Response } from "express";
 import * as consultationService from "../services/consultationServices";
 
 export async function scheduleConsultation(req: Request, res: Response) {
-  res.status(201).send("Scheduled!");
+  const consultation = req.body;
+
+  const result = await consultationService.schedule(
+    consultation.patientName,
+    consultation.phone,
+    consultation.turn,
+    consultation.date
+  );
+
+  res.status(201).send(result);
 }
