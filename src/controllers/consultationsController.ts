dec160a8157ea2fd4c038618ab60bd25b1435fd4
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-10-04 20:20:07 -0300

    fix: added userId for relations with clinic

--- src/controllers/consultationsController.ts
+++ src/controllers/consultationsController.ts
@@ -3,13 +3,25 @@ import * as consultationService from "../services/consultationServices";
 
 export async function scheduleConsultation(req: Request, res: Response) {
   const consultation = req.body;
+  const { id: userId } = res.locals.userData;
 
   const result = await consultationService.schedule(
-    consultation.patientName,
+    consultation.name,
+    consultation.health_insurance,
     consultation.phone,
     consultation.turn,
-    consultation.date
+    consultation.date,
+    userId
   );
 
   res.status(201).send(result);
+}
+
+export async function getConsultationsByDay(req: Request, res: Response) {
+  const { date } = req.params;
+  const { id } = res.locals.userData;
+
+  const result = await consultationService.getConsultationsByDay(date, id);
+
+  res.status(200).send(result);
 }
