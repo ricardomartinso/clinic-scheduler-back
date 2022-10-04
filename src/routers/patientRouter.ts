e29289c8dbc975735401097983712fdbbffcbcde
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-10-04 20:22:49 -0300

    feat: added authentication for patient route

--- src/routers/patientRouter.ts
+++ src/routers/patientRouter.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { scheduleConsultation } from "../controllers/consultationsController";
 import { registerPatient } from "../controllers/patientsController";
+import authenticateToken from "../middlewares/authenticateToken";
 import { validateSchemaMiddleware } from "../middlewares/schemaMiddleware";
 import { authSchema } from "../schemas/authSchema";
 import { consultationSchema } from "../schemas/consultationSchema";
@@ -9,6 +10,7 @@ const patientRouter = Router();
 
 patientRouter.post(
   "/patient",
+  authenticateToken,
   validateSchemaMiddleware(consultationSchema),
   registerPatient
 );
