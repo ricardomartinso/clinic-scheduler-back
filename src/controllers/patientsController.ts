438ba2ff3fdad537c3f7757fe69e891e6683645f
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-10-04 20:21:26 -0300

    fix: added id for register patients on clinics

--- src/controllers/patientsController.ts
+++ src/controllers/patientsController.ts
@@ -5,8 +5,9 @@ import { CreatePatient } from "../types/patientTypes";
 
 export async function registerPatient(req: Request, res: Response) {
   const patient: CreatePatient = req.body;
+  const { id } = res.locals.userData;
 
-  await patientRegister(patient);
+  await patientRegister(patient, id);
 
   res.status(201).send("Patient created!");
 }
