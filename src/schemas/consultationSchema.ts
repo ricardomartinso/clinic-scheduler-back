edcf01360bdf396402adb2f865b0468dd93256c1
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-10-04 20:24:01 -0300

    fix: changed variable name and added health insurance for schema

--- src/schemas/consultationSchema.ts
+++ src/schemas/consultationSchema.ts
@@ -1,7 +1,8 @@
 import Joi from "joi";
 
 export const consultationSchema = Joi.object({
-  patientName: Joi.string().required(),
+  name: Joi.string().required(),
+  health_insurance: Joi.string(),
   phone: Joi.number().required(),
   date: Joi.date().required(),
   turn: Joi.string().required(),
